dbadc8dda8672db812a08c7923f9778640f0ddbe
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2018-10-06 20:29:32 +0300

    helpers renamed to emitters, underscored methods added for inheritance purposes, to added to scoeps

--- SocketIONamespaceController.js
+++ SocketIONamespaceController.js
@@ -1,35 +1,43 @@
 const controllers = {};
 const socketMiddlewares = [];
 class SocketIONamespaceController {
-  constructor(io, namespace, { methods, helpers }) {
+  constructor(io, namespace, { methods, emitters }) {
     controllers[namespace] = this;
     this.io = io;
     this.methods = methods || {};
-    this.helpers = helpers || {};
+    this.emitters = emitters || {};
     this.namespace = io.of(namespace);
     this.methodsKeys = Object.keys(methods);
-    this.helpersKeys = Object.keys(helpers);
+    this.emittersKeys = Object.keys(emitters);
     this.namespace.on('connect', ({ id, use, on, emit, broadcast: { emit: broadcast } }) => {
       const [ , connectionId ] = id.split('#');
       const namespaceScopes = {};
-      const namespaceHelpers = {};
-      const prepareHelpers = (controller, that) => controller.helpersKeys.reduce((accumulator, key) => {
-        accumulator[key] = controller.helpers[key].bind(that);
+      const namespaceEmitters = {};
+      const prepareEmitters = (controller, that) => controller.emittersKeys.reduce((accumulator, key) => {
+        accumulator[key] = controller.emitters[key].bind(that);
         return accumulator;
       }, {});
+      const to = namespace => {
+        const controller = controllers[namespace];
+        const that = namespaceScopes[namespace] ||
+          (namespaceScopes[namespace] = {
+            emit: this.namespace.to(namespace + '#' + connectionId).emit,
+            broadcast: controller.namespace.emit,
+            to
+          });
+        return namespaceEmitters[namespace] || (namespaceEmitters[namespace] = prepareEmitters(controller, that));
+      };
       const that = {
         emit,
         broadcast,
-        helpers: prepareHelpers(this, { emit }),
-        to: namespace => {
-          const controller = controllers[namespace];
-          const that = namespaceScopes[namespace] ||
-            (namespaceScopes[namespace] = { emit: this.namespace.to(namespace + '#' + connectionId).emit });
-          return namespaceHelpers[namespace] || (namespaceHelpers[namespace] = prepareHelpers(controller, that));
-        }
+        emitters: prepareEmitters(this, { emit, broadcast, to }),
+        to
       };
       for (let middleware of socketMiddlewares) use(middleware.bind(that));
-      for (let method of this.methodsKeys) on(this.methods[method].bind(that));
+      for (let method of this.methodsKeys) {
+        that[method] = this.methods[method].bind(that);
+        if (method[0] !== '_') on(method, that[method]);
+      }
     });
   }
   
