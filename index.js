b9593d501ff8065cdbae1382944444e5e30ade42
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2018-10-20 16:16:17 +0300

    added mapGetters helper

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const namespaces = {};
+const mapGetters = (map, prefix) => Object.keys(map).reduce((accumulator, entity) => {
+  prefix = prefix || 'load';
+  const event = prefix + entity[ 0 ].toUpperCase() + entity.slice(1);
+  const resolver = map[ entity ];
+  accumulator[ event ] = async function (payload, ack) {
+    payload = payload || {};
+    payload[ entity ] = await resolver(payload);
+    if (ack) ack(payload[ entity ]);
+    else this.emit(entity, payload);
+  };
+  return accumulator;
+}, {});
 const prepare = (keys, target, socket, listen) => keys.reduce((acc, key) =>
   (acc[ key ] = target[ key ].bind(socket)) && (!listen || (key[ 0 ] === '_') || socket.on(key, acc[ key ])) && acc, {});
 function setupController(io, name, { methods = {}, emitters = {}, connected, created }) {
@@ -15,5 +27,6 @@ function setupController(io, name, { methods = {}, emitters = {}, connected, cre
   });
 }
 module.exports = {
-  driver: io => (namespace, definition) => setupController(io, namespace, definition)
+  driver: io => (namespace, definition) => setupController(io, namespace, definition),
+  mapGetters
 };
